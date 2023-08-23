e630e69e0774a60c9077d40790315c6e605c9db9
NguyenNVPK02647 <nguyennvpk02647@fpt.edu.vn>
2023-08-23 19:55:43 +0700

    duma
    Nguyen dep trai

--- buyzzle/src/pages/home/home.jsx
+++ buyzzle/src/pages/home/home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <>
       <div className="justify-center mt-5">
-        <h1 className="text-3xl text-white font-bold items-center justify-center ">Con me may thang frontend</h1>
+        <h1 className="text-3xl text-white font-bold items-center justify-center ">frontend cc</h1>
       </div>
     </>
   );
