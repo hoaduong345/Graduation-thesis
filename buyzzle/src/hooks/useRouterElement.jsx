395b6d802369bf4dab652c589f9abfeefe36415a
Hoa <vanhoa284@gmail.com>
2023-08-23 10:00:45 +0700

    second commit

---
+++ buyzzle/src/hooks/useRouterElement.jsx
@@ -0,0 +1,26 @@
+import { useRoutes } from "react-router-dom";
+import MainLayout from "../layout/mainLayout";
+// import path from "../servies/path";
+import Home from "../pages/home/Home";
+
+export default function useRouterElement() {
+    const routes = useRoutes([
+      {
+        path: "/",
+        element: (
+          <MainLayout>
+            <Home />
+          </MainLayout>
+        ),
+      },
+      // {
+      //   path: path.details,
+      //   element: (
+      //     <MainLayout>
+      //       <Details />
+      //     </MainLayout>
+      //   ),
+      // },
+    ]);
+  return routes;
+}
