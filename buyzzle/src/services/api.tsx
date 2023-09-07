9e04c7208735d1b66b34699fd66c4be41560e572
CongTuan <101179328+DinhCongTuan7703@users.noreply.github.com>
2023-09-07 15:47:11 +0700

    .

--- buyzzle/src/services/api.tsx
+++ buyzzle/src/services/api.tsx
@@ -1,18 +1,33 @@
-import { CREATE_USER } from "../utils/constants";
+import {
+  CREATE_USER,
+  LOGIN_USER
+} from "../utils/constants";
 import axios, { AxiosResponse } from "axios";
 
 interface UserData {
-    email : string,
-    username : string,
-    password : string,
-    name : string ,
-    
-   
+  email: string,
+  username: string,
+  password: string,
+  name: string,
+}
+
+interface LoginData {
+  username: string;
+  password: string;
 }
 
 export const CreateUser = async (data: UserData): Promise<AxiosResponse> => {
   try {
     const response = await axios.post(CREATE_USER, data);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const loginUser = async (data: LoginData): Promise<AxiosResponse> => {
+  try {
+    const response = await axios.post(LOGIN_USER, data);
     return response;
   } catch (error) {
     throw error;
