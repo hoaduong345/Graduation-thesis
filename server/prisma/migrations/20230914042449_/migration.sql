046f8a46c610d071c58bb4b8a27476bfb7d6af2b
CongTuan <101179328+DinhCongTuan7703@users.noreply.github.com>
2023-09-14 11:25:57 +0700

    .

--- server/prisma/migrations/20230914042449_/migration.sql
+++ server/prisma/migrations/20230914042449_/migration.sql
@@ -24,6 +24,7 @@ CREATE TABLE `Token` (
     `token` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    UNIQUE INDEX `Token_userid_key`(`userid`),
     PRIMARY KEY (`tokenid`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
