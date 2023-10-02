6fe4e4153e02da7485a44680bfc2da78508777f6
Hoa <vanhoa284@gmail.com>
2023-10-02 10:46:41 +0700

    .

--- server/index.js
+++ server/index.js
@@ -22,7 +22,10 @@ app.listen(process.env.APP_PORT = 5000, () => {
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true, 
+}));
 app.use(morgan("common"));
 app.use(cookieParser());
 
