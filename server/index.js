713cd7612da6fa6b8c37e0a22d098c578cc477c9
nham215 <142221006+nham215@users.noreply.github.com>
2023-10-02 17:05:16 +0700

    cookies

--- server/index.js
+++ server/index.js
@@ -1,42 +1,50 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const AuthRouter = require('./routes/AuthRoutes');
+const CartRouter = require('./routes/CartRoutes');
 
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors")
-const morgan = require("morgan")
-const path = require('path')
-const bodyParser = require("body-parser");
-const AuthRouter = require("./routes/AuthRoutes")
-const CartRouter = require("./routes/CartRoutes")
+const UserRouter = require('./routes/UserRoutes');
 
-const UserRouter = require("./routes/UserRoutes")
-
-const ProductRoutes = require("./routes/ProductRoutes")
-const cookieParser = require("cookie-parser");
+const ProductRoutes = require('./routes/ProductRoutes');
+const cookieParser = require('cookie-parser');
 dotenv.config();
 
 const app = express();
 
-app.listen(process.env.APP_PORT = 5000, () => {
+app.listen((process.env.APP_PORT = 5000), () => {
     console.log('Server up and running....');
 });
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
-app.use(morgan("common"));
+const whitelist = ['http://localhost:5173'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(morgan('common'));
 app.use(cookieParser());
 
-app.use(path.join(__dirname, ""), express.static(path.join(__dirname, "")))
+app.use(path.join(__dirname, ''), express.static(path.join(__dirname, '')));
 
-app.use(express.static(path.join(__dirname, "")));
+app.use(express.static(path.join(__dirname, '')));
 
-app.use("/buyzzle/auth", AuthRouter)
+app.use('/buyzzle/auth', AuthRouter);
 
-app.use("/buyzzle/user", UserRouter)
+app.use('/buyzzle/user', UserRouter);
 
 // sản phẩm
-app.use("/buyzzle/product", ProductRoutes)
+app.use('/buyzzle/product', ProductRoutes);
 // CART
-app.use("/buyzzle/cart", CartRouter)
-
-
+app.use('/buyzzle/cart', CartRouter);
