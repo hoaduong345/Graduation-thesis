b6bc1fcd2a378a2e578ce00eb9f5d6fe7668fb87
Hoa <vanhoa284@gmail.com>
2023-10-09 16:07:46 +0700

    .delete Item and Cart

--- server/routes/CartRoutes.js
+++ server/routes/CartRoutes.js
@@ -1,7 +1,12 @@
 const CartController = require("../controller/CartController");
 const router = require("express").Router();
 
+
 router.post("/",CartController.addToCart)
 router.get("/",CartController.getCart)
+// DELETE ITEM
+router.delete("/:id", CartController.deleteItem)
+// DELETE all item on cart
+router.delete("/", CartController.deleteCart)
 
 module.exports = router;
