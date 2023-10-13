11a8c93df5cc364bd6d0a70f87b54cece9a0a127
nham215 <142221006+nham215@users.noreply.github.com>
2023-10-13 09:25:50 +0700

    s

--- buyzzle/src/pages/home/User/CartPage/Cart.tsx
+++ buyzzle/src/pages/home/User/CartPage/Cart.tsx
@@ -77,7 +77,7 @@ export default function Cart() {
          setProductChecked(Product);
       });
    };
-
+   // asdasd
    const removeAllCart = () => {
       cartControllers.removeAllCart().then(() => {
          getCart();
