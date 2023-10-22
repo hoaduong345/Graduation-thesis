10c78f19fd10e8608453fc7c748d43bde0a625cb
Thangbmt79 <nguyenvietthang5626@gmail.com>
2023-10-22 10:16:18 +0700

    ss

--- server/controller/Statistics_Controller.js
+++ server/controller/Statistics_Controller.js
@@ -3,57 +3,32 @@ const prisma = new PrismaClient();
 
 const StatisticsController = {
     getStatictics: async (req, res) => {
-        //     try {
-        //         const allOrderDetails = await prisma.orderDetail.findMany({
-        //             include: {
-        //                 fK_productOrder: {
-        //                     select: {
-        //                         id: true,
-        //                         name: true,
-        //                         price: true,
-        //                     },
-        //                 },
-        //             },
-        //         });
-
-        //         const productStats = {};
-
-        //         allOrderDetails.forEach((orderDetail) => {
-        //             const product = orderDetail.fK_productOrder;
-        //             const createdAt = orderDetail.createdAt.toDateString(); // Lấy ngày tạo đơn hàng
-
-        //             if (!productStats[createdAt]) {
-        //                 productStats[createdAt] = [];
-        //             }
-
-        //             productStats[createdAt].push({
-        //                 id: product.id,
-        //                 name: product.name,
-        //                 price: product.price,
-        //             });
-        //         });
-
-        //         res.json(productStats);
-        //     } catch (error) {
-        //         console.error('Error:', error);
-        //         res.status(500).json({ error: 'An error occurred while fetching product stats.' });
-        //     }
-        // },
         try {
+            const { date } = req.query; // Ngày cần thống kê (ví dụ: '2023-10-01')
+            // Chuyển ngày thành dạng Date object
+            const selectedDate = new Date(date);
+
+            // Tính ngày đầu và ngày cuối tháng từ ngày đã chọn
+            const firstDayOfMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
+            const lastDayOfMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0);
+
+            // Truy vấn cơ sở dữ liệu để lấy tất cả OrderDetails trong khoảng thời gian đã chọn
             const allOrderDetails = await prisma.orderDetail.findMany({
-                include: {
-                    fK_productOrder: {
-                        select: {
-                            id: true,
-                            name: true,
-                            price: true,
-                        },
+                where: {
+                    createdAt: {
+                        gte: firstDayOfMonth,
+                        lte: lastDayOfMonth,
                     },
+                },
+                include: {
+                    fK_productOrder: true, // Lấy thông tin sản phẩm
                 },
             });
 
+            // Khởi tạo đối tượng thống kê
             const productStats = {};
 
+            // Lặp qua danh sách OrderDetails và tính toán thông tin thống kê
             allOrderDetails.forEach((orderDetail) => {
                 const product = orderDetail.fK_productOrder;
                 const createdAt = orderDetail.createdAt.toDateString(); // Lấy ngày tạo đơn hàng
@@ -66,17 +41,19 @@ const StatisticsController = {
                 const existingProduct = productStats[createdAt].find((item) => item.id === product.id);
 
                 if (existingProduct) {
-                    // Nếu sản phẩm đã tồn tại, cập nhật số lượng và tổng tiền
+                    // Nếu sản phẩm đã tồn tại, cập nhật số lượng, tổng tiền và số lượng sản phẩm
                     existingProduct.quantity++;
-                    existingProduct.totalPrice += product.price;
+                    existingProduct.totalPrice += product.sellingPrice;
+                    existingProduct.totalQuantity += 1;
                 } else {
                     // Nếu sản phẩm chưa tồn tại, thêm sản phẩm vào thông tin thống kê
                     productStats[createdAt].push({
                         id: product.id,
                         name: product.name,
-                        price: product.price,
+                        sellingPrice: product.sellingPrice,
                         quantity: 1,
-                        totalPrice: product.price,
+                        totalPrice: product.sellingPrice,
+                        totalQuantity: 1,
                     });
                 }
             });
@@ -87,6 +64,34 @@ const StatisticsController = {
             res.status(500).json({ error: 'An error occurred while fetching product stats.' });
         }
     },
+    //     try {
+    //         const { date } = req.query; // Ngày cần thống kê (ví dụ: '2023-10-01')
+    //         // Chuyển ngày thành dạng Date object
+    //         const selectedDate = new Date(date);
+
+    //         // Tính ngày đầu và ngày cuối tháng từ ngày đã chọn
+    //         const firstDayOfMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
+    //         const lastDayOfMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0);
+
+    //         // Truy vấn cơ sở dữ liệu để thống kê sản phẩm
+    //         const sales = await prisma.$queryRaw`
+    // SELECT p.name AS productName,
+    //        SUM(od.quantity) AS quantitySold,
+    //        SUM(od.price) AS totalRevenue,
+    //        od.createdAt AS saleDate,
+    //        (p.quantity - SUM(od.quantity)) AS remainingQuantity
+    // FROM OrderDetail od
+    // JOIN Product p ON od.productId = p.id
+    // WHERE od.createdAt >= ${firstDayOfMonth} AND od.createdAt <= ${lastDayOfMonth}
+    // GROUP BY p.name, od.createdAt, p.quantity
+    // ORDER BY od.createdAt`;
+
+    //         res.json(sales);
+    //     } catch (error) {
+    //         console.error(error);
+    //         res.status(500).json({ error: 'Internal Server Error' });
+    //     }
+    // },
 };
 
 module.exports = StatisticsController;
