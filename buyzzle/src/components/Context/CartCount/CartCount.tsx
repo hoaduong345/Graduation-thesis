9887a546daaa117946c556fef2887298b7bb3066
Thangbmt79 <nguyenvietthang5626@gmail.com>
2023-10-26 09:50:09 +0700

    link Cart 26/10

--- buyzzle/src/components/Context/CartCount/CartCount.tsx
+++ buyzzle/src/components/Context/CartCount/CartCount.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Ellips from "../../../Assets/TSX/Ellips";
 import Shoppingcart from "../../../Assets/TSX/Shopping-cart";
 import LoadingAddCart from "../../../Helper/Loading/loadingAddCart";
@@ -13,15 +14,17 @@ export default function CartCount() {
             <LoadingAddCart />
           ) : (
             // Hiển thị nội dung khi loading đã kết thúc
-            <div className="items-center flex pr-11 max-[769px]:pr-[10px]">
-              <Shoppingcart />
-              <div className="absolute">
-                <Ellips />
-                <span className="text-white font-bold absolute top-[-21px] ml-[30px] text-xs max-xl:text-[9px] max-xl:absolute max-xl:mr-[100px]">
-                  {carts.item?.length ?? 0}
-                </span>
+            <Link to={"/cart"}>
+              <div className="items-center flex pr-11 max-[769px]:pr-[10px]">
+                <Shoppingcart />
+                <div className="absolute">
+                  <Ellips />
+                  <span className="text-white font-bold absolute top-[-21px] ml-[30px] text-xs max-xl:text-[9px] max-xl:absolute max-xl:mr-[100px]">
+                    {carts.item?.length ?? 0}
+                  </span>
+                </div>
               </div>
-            </div>
+            </Link>
           )}
         </div>
       }
