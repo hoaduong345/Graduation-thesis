a8f046400ecaf0daa26baf79b0ea6c835d031d0b
nham215 <142221006+nham215@users.noreply.github.com>
2023-10-27 10:58:20 +0700

    ui detail order admin 80%

--- buyzzle/src/pages/home/Admin/Management/Order/DetailOrderManagement.tsx
+++ buyzzle/src/pages/home/Admin/Management/Order/DetailOrderManagement.tsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Images } from "../../../../../Assets/TS";
 import Orderbuyzzle from "../../../../../Assets/TSX/Orderbuyzzle";
 import { orderControllers } from "../../../../../Controllers/OrderControllers";
+import { formatDateYYYY, numberFormat } from "../../../../../Helper/Format";
+import TimelineStepper from "../../../../../Helper/Stepper/TimelineStepper";
 import { OrderModel } from "../../../../../Model/OrderModel";
 import Container from "../../../../../components/container/Container";
-import SitebarAdmin from "../../Sitebar/Sitebar";
+import BuyzzleOrderAdmin from "../../Assets/TSX/BuyzzleOrderAdmin";
+import MessageOrderAdmin from "../../Assets/TSX/MessageOrderAdmin";
 import PrintOrder from "../../Assets/TSX/PrintOrder";
-import { formatDateYYYY } from "../../../../../Helper/Format";
-import { Images } from "../../../../../Assets/TS";
-import TimelineStepper from "../../../../../Helper/Stepper/TimelineStepper";
+import SitebarAdmin from "../../Sitebar/Sitebar";
+import PhoneOrderAdmin from "../../Assets/TSX/PhoneOrderAdmin";
+import Map from "../../../../../Assets/TSX/Map";
+import Paymethod from "../../Assets/TSX/Paymethod";
 
 export default function DetailOrderManagement() {
    const { id } = useParams();
@@ -48,7 +53,9 @@ export default function DetailOrderManagement() {
                         </div>
                         <div className="flex flex-col gap-[4px] items-center">
                            <p className="py-[1px] px-5 border-[1px] rounded-[10px] bg-[#00B207] text-xs text-white font-bold">
-                              Nhận thanh toán
+                              {order.paymentMethod == "Thẻ tín dụng"
+                                 ? "Đã thanh toán"
+                                 : "Chưa thanh toán"}
                            </p>
                            <p className="text-[#4C4C4C] text-sm">
                               {formatDateYYYY(order.createdAt)}
@@ -110,51 +117,51 @@ export default function DetailOrderManagement() {
                               </div>
 
                               <div className="border-b-[1px] mx-4">
-
-                                 <div className="grid grid-cols-4 pl-2 py-[16px] items-center bg-[#FFFFFF]">
-                                    <div className="col-span-2 text-sm flex gap-4 items-center">
-                                       <img
-                                          className="w-[70px] h-[70px] object-contain"
-                                          src={Images.imageproduct}
-                                          alt=""
-                                       />
-                                       <div>
-                                          <p className="text-base font-medium text-[#393939] max-[870px]:text-[13px]">
-                                             Sản phẩ, xiij jj
-                                          </p>
-                                          <p className="text-sm text-[#1A1A1A] font-medium max-[870px]:text-[13px]">
-                                             SL:{" "}
-                                             <span className="text-[#4C4C4C]">
-                                                x2
-                                             </span>
-                                          </p>
-                                       </div>
-                                    </div>
-                                    <div className="col-span-1 flex gap-1 justify-around items-center">
-                                       <p className="font-medium text-[#7A828A] text-sm line-through max-[870px]:text-[13px]">
-                                          10.000đ
-                                       </p>
-                                       <p className="font-medium text-[#1A1A1A] text-base max-[870px]:text-[13px]">
-                                          10.000đ
-                                       </p>
-                                    </div>
-                                    <div className="col-span-1">
-                                       <p className="font-medium text-[#EA4B48] text-base text-center max-[870px]:text-[13px]">
-                                          10.000đ
-                                       </p>
-                                    </div>
-                                 </div>
-
+                                 {order?.OrderDetail?.map((e) => {
+                                    return (
+                                       <>
+                                          <div className="grid grid-cols-4 pl-2 py-[16px] items-center bg-[#FFFFFF]">
+                                             <div className="col-span-2 text-sm flex gap-4 items-center">
+                                                <img
+                                                   className="w-[70px] h-[70px] object-contain"
+                                                   src={e.image}
+                                                   alt=""
+                                                />
+                                                <div>
+                                                   <p className="text-base font-medium text-[#393939] max-[870px]:text-[13px]">
+                                                      {e.name}
+                                                   </p>
+                                                   <p className="text-sm text-[#1A1A1A] font-medium max-[870px]:text-[13px]">
+                                                      SL:{" "}
+                                                      <span className="text-[#4C4C4C]">
+                                                         x{e.quantity}
+                                                      </span>
+                                                   </p>
+                                                </div>
+                                             </div>
+                                             <div className="col-span-1 flex justify-center items-center">
+                                                <p className="font-medium text-[#1A1A1A] text-base max-[870px]:text-[13px]">
+                                                   {numberFormat(e.price)}
+                                                </p>
+                                             </div>
+                                             <div className="col-span-1">
+                                                <p className="font-medium text-[#EA4B48] text-base text-center max-[870px]:text-[13px]">
+                                                   {numberFormat(e.total)}
+                                                </p>
+                                             </div>
+                                          </div>
+                                       </>
+                                    );
+                                 })}
                               </div>
-                              <div className="flex justify-end mt-6 px-4">
+                              <div className="flex justify-end mt-6 px-16">
                                  <div className="w-[270px] flex flex-col gap-3">
                                     <div className="flex justify-between">
                                        <p className="text-sm font-medium text-[#393939] max-[870px]:text-[11px]">
                                           Tổng Giá Sản Phẩm:{" "}
                                        </p>
                                        <p className="text-sm text-[#EA4B48] max-[870px]:text-[11px]">
-                                          {/* {numberFormat(orderDetails.subtotal)} */}
-                                          10.000đ
+                                          {numberFormat(order.subtotal)}
                                        </p>
                                     </div>
                                     <div className="flex justify-between border-t-[1px] pt-2">
@@ -163,8 +170,7 @@ export default function DetailOrderManagement() {
                                        </p>
                                        <div className="flex gap-1">
                                           <p className="text-sm text-[#FFAAAF] line-through max-[870px]:text-[11px]">
-                                             {/* {numberFormat(orderDetails.discount)} */}
-                                             10.000đ
+                                             {numberFormat(order.discount)}
                                           </p>
                                        </div>
                                     </div>
@@ -174,8 +180,7 @@ export default function DetailOrderManagement() {
                                        </p>
                                        <div className="flex gap-1">
                                           <p className="text-sm text-[#EA4B48] max-[870px]:text-[11px]">
-                                             {/* {numberFormat(orderDetails.shipping)} */}
-                                             30.000đ
+                                             {numberFormat(order.shipping)}
                                           </p>
                                        </div>
                                     </div>
@@ -184,13 +189,11 @@ export default function DetailOrderManagement() {
                                           Tổng Thanh Toán:{" "}
                                        </p>
                                        <p className="text-xl text-[#EA4B48] font-semibold max-[870px]:text-sm">
-                                          {/* {numberFormat(orderDetails.amountTotal)} */}
-                                          10.000đ
+                                          {numberFormat(order.amountTotal)}
                                        </p>
                                     </div>
                                  </div>
                               </div>
-
                            </div>
                         </div>
                         <div className="py-8 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
@@ -202,10 +205,100 @@ export default function DetailOrderManagement() {
                            </div>
                         </div>
                      </div>
+
                      <div className="col-span-1">
-                        <div className="flex flex-col gap-5 py-8 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
-                           <div className="px-5 text-xl font-semibold text-[#393939]">
+                        <div className="flex flex-col gap-5 px-5 py-8 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
+                           <div className="text-xl font-semibold text-[#393939]">
                               <p>Thông Tin User</p>
+                           </div>
+
+                           <div className="flex justify-between mb-8">
+                              <div className="flex gap-1">
+                                 <img
+                                    className="w-[40px] h-[40px] object-cover rounded-full border-4"
+                                    src={Images.avatar_admin}
+                                    alt=""
+                                 />
+
+                                 <div>
+                                    <p className="text-sm">
+                                       {order?.User?.name.substring(0, 11)}{" "}
+                                    </p>
+
+                                    <p className="text-[#12b004] text-[10px]">
+                                       {order?.User?.email.substring(0, 11)}{" "}
+                                    </p>
+                                 </div>
+                              </div>
+
+                              <div className="flex flex-col gap-3">
+                                 <div className="flex justify-end items-centers">
+                                    <button
+                                       className="text-white text-center text-xs font-bold
+                 bg-[#EA4B48] hover:bg-[#ff6d65] mt-2
+                 rounded-md transition duration-150 cursor-pointer
+                 flex items-center px-2 py-1"
+                                    >
+                                       <p className="w-full">Chat</p>
+                                       <MessageOrderAdmin />
+                                    </button>
+                                 </div>
+                                 <div className=" flex gap-1 items-center text-[10px] text-[#7A828A]">
+                                    <BuyzzleOrderAdmin />
+                                    <p>20 Đơn hàng</p>
+                                 </div>
+                              </div>
+                           </div>
+
+                           <div className="flex flex-col gap-5 mb-10">
+                              <p className="text-xl font-semibold text-[#393939]">
+                                 Địa Chỉ Nhận Hàng
+                              </p>
+
+                              <div className="flex flex-col gap-6">
+                                 <div className="flex flex-col gap-1">
+                                    <div className="flex gap-2 items-center text-[#4C4C4C] font-medium text-sm">
+                                       <PhoneOrderAdmin />
+                                       <p>Phone</p>
+                                    </div>
+                                    <p className="pl-2 border-l-[1px] border-[#FFAAAF] font-semibold text-[#5D5FEF] text-sm">
+                                       {order?.User?.phonenumber}
+                                    </p>
+                                 </div>
+
+                                 <div className="flex flex-col gap-1">
+                                    <div className="flex gap-2 items-center text-[#4C4C4C] font-medium text-sm">
+                                       <Map />
+                                       <p>Địa Chỉ</p>
+                                    </div>
+                                    <div className="pl-2 border-l-[1px] border-[#FFAAAF] font-semibold">
+                                       <p className="text-[#1A1A1A] text-sm">
+                                          {order?.User?.name}
+                                       </p>
+                                       <p className="text-[#4C4C4C] text-sm">
+                                          {order?.User?.address}
+                                       </p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+
+                           <div className="flex flex-col gap-5">
+                              <p className="text-xl font-semibold text-[#393939]">
+                                 Thanh Toán
+                              </p>
+
+                              <div className="flex flex-col gap-6">
+                                 <div className="flex flex-col gap-1">
+                                    <div className="flex gap-2 items-center text-[#4C4C4C] font-medium text-sm">
+                                       <Paymethod />
+                                       <p>Phương thức thanh toán</p>
+                                    </div>
+                                    <p className="pl-2 border-l-[1px] border-[#FFAAAF] font-semibold text-[#5D5FEF] text-sm">
+                                       {order?.paymentMethod}
+                                    </p>
+                                 </div>
+                              </div>
                            </div>
                         </div>
                      </div>
